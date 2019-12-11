93f68cd0d98dbd16f2c174ef2d8785f6ad83f582
Mohammad Khattab <MOHAMMAD.KHATTAB.987@gmail.com>
2019-12-11 13:28:37 +0100

    Add Controller

---
+++ Garage 2.0/Data/Garage_2_0Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Garage_2._0.Models
+{
+    public class Garage_2_0Context : DbContext
+    {
+        public Garage_2_0Context (DbContextOptions<Garage_2_0Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Garage_2._0.Models.Vehicle> Vehicle { get; set; }
+    }
+}
