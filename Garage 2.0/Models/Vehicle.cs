2c1e2ea7bc979cd563adccc35604b20fe3a52e02
Mohammad Khattab <MOHAMMAD.KHATTAB.987@gmail.com>
2019-12-11 13:26:23 +0100

    Adding Vehicle Model

---
+++ Garage 2.0/Models/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_2._0.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string RegNr { get; set; }
+
+        public Typ Typ { get; set; }
+        public DateTime TimeOfParking { get; set; }
+        public int NumnOfWheels { get; set; }
+
+        public String Color { get; set; }
+        public String Model { get; set; }
+        public String Brand { get; set; }
+    }
+
+    public enum Typ
+    {
+        Car,
+        Boat,
+        Buss,
+        Airplane
+    }
+}
