eb385498e81ad9157e03340608959824e9655dd0
Mikael <test.kurs@hotmail.net>
2019-12-11 15:10:19 -0800

    Added MockVehicleRepository

---
+++ Garage 2.0/Models/IVehicleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_2._0.Models
+{
+    public interface IVehicleRepository
+    {
+        IEnumerable<Vehicle> AllVehicles { get; }
+        Vehicle GetVehicleById(int Id);
+        Vehicle GetVehiclebyRegNr(string RegNr);
+
+    }
+}
---
+++ Garage 2.0/Models/MockVehicleRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_2._0.Models
+{
+    public class MockVehicleRepository : IVehicleRepository
+    {
+        public IEnumerable<Vehicle> AllVehicles =>
+            new List<Vehicle>
+            {
+                new Vehicle {Id=1, RegNr="ABC123", Typ=Typ.Car, TimeOfParking=DateTime.Now, NumnOfWheels=4, Color="Green", Model="Stationwagon", Brand="Volvo" },
+                new Vehicle {Id=2, RegNr="BCD234", Typ=Typ.Buss, TimeOfParking=DateTime.Now, NumnOfWheels=8, Color="Red", Model="DoubleDecker", Brand="Scania" },
+                new Vehicle {Id=3, RegNr="CDE345", Typ=Typ.Airplane, TimeOfParking=DateTime.Now, NumnOfWheels=6, Color="Silver", Model="DC10", Brand="Lockheed" },
+                new Vehicle {Id=4, RegNr="DEF456", Typ=Typ.Boat, TimeOfParking=DateTime.Now, NumnOfWheels=0, Color="Brown", Model="Sailingboat", Brand="Penta" },
+            };
+
+        public Vehicle GetVehicleById(int Id)
+        {
+            return AllVehicles.FirstOrDefault(p => p.Id == Id);
+        }
+
+        public Vehicle GetVehiclebyRegNr(string RegNr)
+        {
+            return AllVehicles.FirstOrDefault(p => p.RegNr == RegNr);
+        }
+    }
+}
