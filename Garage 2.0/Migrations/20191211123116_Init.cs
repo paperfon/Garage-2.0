3d3ad76d3a2c681627c2d1c72e42c45185b1fcce
Mohammad Khattab <MOHAMMAD.KHATTAB.987@gmail.com>
2019-12-11 13:31:35 +0100

    Add Migration

---
+++ Garage 2.0/Migrations/20191211123116_Init.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Garage_2._0.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RegNr = table.Column<string>(nullable: true),
+                    Typ = table.Column<int>(nullable: false),
+                    TimeOfParking = table.Column<DateTime>(nullable: false),
+                    NumnOfWheels = table.Column<int>(nullable: false),
+                    Color = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(nullable: true),
+                    Brand = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
