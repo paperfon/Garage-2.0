1e79e450a768bc3593b2d076ad2c7efa443151e3
Mohammad Khattab <MOHAMMAD.KHATTAB.987@gmail.com>
2019-12-18 09:19:43 +0100

    Change Boat to Motorcykel

--- Garage 2.0/Data/Garage_2_0Context.cs
+++ Garage 2.0/Data/Garage_2_0Context.cs
@@ -20,7 +20,7 @@ namespace Garage_2._0.Models
                 new Vehicle { Id = 1, RegNr = "ABC123", Typ = Typ.Car, TimeOfParking = DateTime.Now, NumnOfWheels = 4, Color = "Green", Model = "Stationwagon", Brand = "Volvo" },
                 new Vehicle { Id = 2, RegNr = "BCD234", Typ = Typ.Buss, TimeOfParking = DateTime.Now, NumnOfWheels = 8, Color = "Red", Model = "DoubleDecker", Brand = "Scania" },
                 new Vehicle { Id = 3, RegNr = "CDE345", Typ = Typ.Airplane, TimeOfParking = DateTime.Now, NumnOfWheels = 6, Color = "Silver", Model = "DC10", Brand = "Lockheed" },
-                new Vehicle { Id = 4, RegNr = "DEF456", Typ = Typ.Boat, TimeOfParking = DateTime.Now, NumnOfWheels = 0, Color = "Brown", Model = "Sailingboat", Brand = "Penta" },
+                new Vehicle { Id = 4, RegNr = "DEF456", Typ = Typ.Motorcykel, TimeOfParking = DateTime.Now, NumnOfWheels = 0, Color = "Brown", Model = "Sailingboat", Brand = "Penta" },
                 new Vehicle { Id = 5, RegNr = "ASD345", Typ = Typ.Car, TimeOfParking = DateTime.Parse("1997-02-22"), NumnOfWheels = 34, Color = "Violet", Model = "BladeRunner", Brand = "BMW"}
                 );
         }
--- Garage 2.0/Models/Vehicle.cs
+++ Garage 2.0/Models/Vehicle.cs
@@ -25,7 +25,7 @@ namespace Garage_2._0.Models
     public enum Typ
     {
         Car,
-        Boat,
+        Motorcykel,
         Buss,
         Airplane
     }
